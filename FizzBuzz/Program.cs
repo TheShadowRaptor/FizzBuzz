4d24e6972df98a9982dcb7444875c29ee4181b9f
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2022-10-31 16:09:13 -0300

    [FINISHED] FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,30 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("FizzBuzz");
+            Console.WriteLine("0--------------------------0");
+            Console.WriteLine("");
+
+            for (int i = 0; i < 100; i++)
+            {
+                string output = "";
+
+                Method(3, "Fizz", i, ref output);
+                Method(5, "Buzz", i, ref output);
+
+                if (output == "") output = i.ToString();
+
+                Console.WriteLine(output);
+            }
+            Console.ReadLine();
+
+            // 10/3 = 3.3333
+            Console.WriteLine();
+
         }
+        static void Method(int num, string name, int i, ref string output)
+        {
+            if (i % num == 0) output += name;
+        } 
     }
 }
